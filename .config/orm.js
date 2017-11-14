ef11cf63d93f09212c8b665954d2ce9006dd3b51
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-11-14 11:48:43 -0800

    Setup ORM and require in pizzas model

--- .config/orm.js
+++ .config/orm.js
@@ -1,1 +1,25 @@
-//orm.js
+var sql = require('./connection');
+
+orm = {
+	selectAll: selectAll,
+	insertOne: insertOne,
+	updateOne: updateOne
+}
+
+module.exports = orm;
+
+function selectAll() {
+	sql.query('SELECT * FROM pizzas', function (error, results) {
+		if (error) throw error;
+		console.log(results);
+		sql.end();
+	});
+}
+
+function insertOne() {
+	console.log("insertOne");
+}
+
+function updateOne(){
+	console.log("updateOne");
+}
