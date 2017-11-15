84367df1eb877763095cfcfdbbe1ca2d341f5a13
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-11-15 00:44:32 -0800

    Pizza Bling

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
+const app = express();
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', function (req, res) {
+	res.render('index');
+});
+
+app.listen(3000);
