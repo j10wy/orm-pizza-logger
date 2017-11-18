57ef7c3f1cd594ce2c2a7a93bd1d97bc32fcd333
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-11-17 17:38:01 -0800

    Basic add new pizza function

--- public/assets/js/pizzajax.js
+++ public/assets/js/pizzajax.js
@@ -5,6 +5,12 @@ let notDevoured = document.getElementById("not-devoured");
 // Create a reference to div#devoured element
 let devoured = document.getElementById("devoured");
 
+let submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", function (event) {
+	event.preventDefault();
+	addPizza();
+});
+
 // Use the axios library for HTTP requests
 // This will run on page load.
 function getPizzas() {
@@ -17,19 +23,26 @@ function getPizzas() {
 			// Create a text node that holds the name of each type of pizza
 			let textnode = document.createTextNode(item.pizza_name);
 			// Create a button and listen for click events
-			let button = document.createElement("button");
-			button.addEventListener("click", function () {
-				notDevoured.innerHTML= "";
+			let pizzaDiv = document.createElement("div");
+			pizzaDiv.addEventListener("click", function (event) {
+				event.preventDefault();
+				notDevoured.innerHTML = "";
 				getPizzas();
 			});
 			// Append the text node above to the button element
-			button.appendChild(textnode);
+			pizzaDiv.appendChild(textnode);
 			// Append the button to div#not-devoured
-			notDevoured.appendChild(button);
+			notDevoured.appendChild(pizzaDiv);
 		});
 
 	});
 }
 
+function addPizza() {
+	let pizzaInput = document.getElementById("newPizza").value;
+	// let textnode = document.createTextNode(item.pizza_name);
+	// let pizzaDiv = document.createElement("div");
+	console.log(pizzaInput)
+}
 
-getPizzas();
+//getPizzas();
